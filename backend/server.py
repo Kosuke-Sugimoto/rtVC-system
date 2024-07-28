d6c86e2cef2985cffc248edef4c4ba372237909d
Kosuke-Sugimoto <ks.1285.main@gmail.com>
2024-07-28 19:13:07 +0900

    add simple realtimeio scripts

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,8 @@ import ssl
 import uuid
 
 from aiohttp import web
-from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiohttp_cors import setup, ResourceOptions
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCIceCandidate
 from aiortc.contrib.media import MediaRelay
 from av import AudioFrame
 
@@ -44,6 +45,24 @@ class AudioTransformTrack(MediaStreamTrack):
         後はフレームの順序が崩れることを防止したり、空白が生じることを防止する
         他にもこれを利用することで再生速度の変更やシーク操作の実装などが可能らしい
 
+        new_frame作成の際にはsample_rateを引き継がせておかないと以下のようなエラーが発生
+        [auto_aresample_0 @ 0x7f91f0004f40] [SWR @ 0x7f91f00050c0] Requested input sample rate 0 is invalid
+        [auto_aresample_0 @ 0x7f91f0004f40] Failed to configure output pad on auto_aresample_0
+        WARNING:aiortc.rtcrtpsender:RTCRtpsender(audio) Traceback (most recent call last):
+        File "/usr/local/lib/python3.10/dist-packages/aiortc/rtcrtpsender.py", line 346, in _run_rtp
+            enc_frame = await self._next_encoded_frame(codec)
+        File "/usr/local/lib/python3.10/dist-packages/aiortc/rtcrtpsender.py", line 293, in _next_encoded_frame
+            payloads, timestamp = await self.__loop.run_in_executor(
+        File "/usr/lib/python3.10/concurrent/futures/thread.py", line 58, in run
+            result = self.fn(*self.args, **self.kwargs)
+        File "/usr/local/lib/python3.10/dist-packages/aiortc/codecs/opus.py", line 82, in encode
+            for frame in self.resampler.resample(frame):
+        File "av/audio/resampler.pyx", line 34, in av.audio.resampler.AudioResampler.resample
+        File "av/audio/resampler.pyx", line 90, in av.audio.resampler.AudioResampler.resample
+        File "av/filter/graph.pyx", line 42, in av.filter.graph.Graph.configure
+        File "av/error.pyx", line 326, in av.error.err_check
+        av.error.ValueError: [Errno 22] Invalid argument
+
         参考：
         - https://pyav.org/docs/develop/api/audio.html#module-av.audio.frame
         - https://pyav.org/docs/develop/api/frame.html#av.frame.Frame
@@ -51,9 +70,10 @@ class AudioTransformTrack(MediaStreamTrack):
 
         npy_frame = frame.to_ndarray() * 2
 
-        new_frame = AudioFrame.from_ndarray(npy_frame, format=frame.format)
+        new_frame = AudioFrame.from_ndarray(npy_frame, format=frame.format.name) # nameまで指定しないとオブジェクトのまま
         new_frame.pts = frame.pts
         new_frame.time_base = frame.time_base
+        new_frame.sample_rate = frame.sample_rate
 
         return new_frame
 
@@ -67,100 +87,140 @@ async def offer(request):
     """
 
     params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-
-    pc = RTCPeerConnection()
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
-    pcs.add(pc)
+    logger.info("Received %s from %s", params["type"], request.remote)
 
-    def log_info(msg, *args):
-        logger.info(pc_id + " " + msg, *args)
+    if params["type"] == "offer":
+        offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    log_info("Created for %s", request.remote)
+        pc = RTCPeerConnection()
+        pcs.add(pc)
 
-    # MediaPlayerはファイルなどからビデオ・オーディオを読み取る関数
-    #  ファイル、URL、ウェブカメラ等から読み取れるっぽい
-    #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaPlayer
-    # MediaRecorderはビデオ・オーディオをファイルに書き込む関数
-    #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaRecorder
-    # MediaBlackholeはソースを受け取って破棄するためのもの(例外処理用…？)
-    #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaBlackhole
-    # Playerは'media source'、Recorderは'media sink'と書かれていたがこれはいったい…？
+        def log_info(msg, *args):
+            logger.info(pc_id + " " + msg, *args)
 
-    # TODO : 要らなさそうなら消す
-    # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
-    # if args.record_to:
-    #     recorder = MediaRecorder(args.record_to)
-    # else:
-    #     recorder = MediaBlackhole()
+        log_info("Created for %s", request.remote)
 
-    # pc.on(...)はイベントリスナー、callback関数を定義しそれを実行する
-    @pc.on("datachannel")
-    def on_datachannel(channel):
-        @channel.on("message")
-        def on_message(message):
-            if isinstance(message, str) and message.startswith("ping"):
-                channel.send("pong" + message[4:])
+        # MediaPlayerはファイルなどからビデオ・オーディオを読み取る関数
+        #  ファイル、URL、ウェブカメラ等から読み取れるっぽい
+        #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaPlayer
+        # MediaRecorderはビデオ・オーディオをファイルに書き込む関数
+        #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaRecorder
+        # MediaBlackholeはソースを受け取って破棄するためのもの(例外処理用…？)
+        #  参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaBlackhole
+        # Playerは'media source'、Recorderは'media sink'と書かれていたがこれはいったい…？
 
-    @pc.on("icecandidate")
-    def on_icecandidate(event):
-        """
-        aiortcライブラリが内部で管理するみたいなのでサーバー側では明示的に指定する必要はない
-        ログの設定をしたり処理の追加をする際にはこのように設定が可能
+        # TODO : 要らなさそうなら消す
+        # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+        # if args.record_to:
+        #     recorder = MediaRecorder(args.record_to)
+        # else:
+        #     recorder = MediaBlackhole()
 
-        何やらICEに関してConnecting...のまま推移しない問題も発生するみたい
-        https://github.com/aiortc/aiortc/issues/1084
-        一応デフォルトのままやれば問題なさそうだが、細かく設定するとダメなのかな…？(読んだ当時では知識不足)
-        """
+        # pc.on(...)はイベントリスナー、callback関数を定義しそれを実行する
+        @pc.on("datachannel")
+        def on_datachannel(channel):
+            @channel.on("message")
+            def on_message(message):
+                if isinstance(message, str) and message.startswith("ping"):
+                    channel.send("pong" + message[4:])
 
-        if event.candidate:
-            log_info("ICE Candidate: %s", event.candidate)
+        @pc.on("icecandidate")
+        def on_icecandidate(event):
+            """
+            aiortcライブラリが内部で管理するみたいなのでサーバー側では明示的に指定する必要はない
+            ログの設定をしたり処理の追加をする際にはこのように設定が可能
 
-    @pc.on("connectionstatechange")
-    async def on_connectionstatechange():
-        log_info("Connection state is %s", pc.connectionState)
-        if pc.connectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
+            何やらICEに関してConnecting...のまま推移しない問題も発生するみたい
+            https://github.com/aiortc/aiortc/issues/1084
+            一応デフォルトのままやれば問題なさそうだが、細かく設定するとダメなのかな…？(読んだ当時では知識不足)
+            
+            Connecting...のまま推移しない問題が発生したが、原因としてはクライアント側でsetRemoteDescriptionしていなかったことだった模様
+            """
 
-    @pc.on("track")
-    def on_track(track):
-        """
-        relay.subscribeはトラックを複製するためのメソッド
-        複数クライアント・複数種の異なる変換処理を施したいときに役立つ
-        　⇒YouTubeとかの配信みたいな…？
-        クライアントとサーバーが1対1なら必ずしも必要ないような…？
-        参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaRelay
-        """
+            if event.candidate:
+                log_info("ICE Candidate: %s", event.candidate)
 
-        log_info("Track %s received", track.kind)
+        @pc.on("connectionstatechange")
+        async def on_connectionstatechange():
+            log_info("Connection state is %s", pc.connectionState)
+            if pc.connectionState == "failed":
+                await pc.close()
+                pcs.discard(pc)
 
-        if track.kind == "audio":
-            pc.addTrack(AudioTransformTrack(relay.subscribe(track)))
+        @pc.on("track")
+        def on_track(track):
+            """
+            relay.subscribeはトラックを複製するためのメソッド
+            複数クライアント・複数種の異なる変換処理を施したいときに役立つ
+            　⇒YouTubeとかの配信みたいな…？
+            クライアントとサーバーが1対1なら必ずしも必要ないような…？
+            参考：https://aiortc.readthedocs.io/en/latest/helpers.html#aiortc.contrib.media.MediaRelay
+            """
 
-        # TODO：要らなさそうなら消す
-        # @track.on("ended")
-        # async def on_ended():
-        #     log_info("Track %s ended", track.kind)
-        #     await recorder.stop()
+            log_info("Track %s received", track.kind)
 
-    # SDP(Session Description Protocol)を設定する
-    # remote: client, local: server (server.py視点)
-    # オファーをremoteから受け取った際にはremoteの設定？をlocalに登録するそうな
-    await pc.setRemoteDescription(offer)
+            if track.kind == "audio":
+                pc.addTrack(AudioTransformTrack(relay.subscribe(track)))
 
-    # オファーに対する回答(Answer SDP)を作成
-    # localで生成した回答をremoteに登録(注意!!：登録しただけで送信はしていない)
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
+            # TODO：要らなさそうなら消す
+            # @track.on("ended")
+            # async def on_ended():
+            #     log_info("Track %s ended", track.kind)
+            #     await recorder.stop()
 
-    # remoteに登録した回答(Answer 'SDP')をremoteに送信する
-    return web.Response(
-        content_type="application/json",
-        text=json.dumps(
-            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-        ),
-    )
+        # SDP(Session Description Protocol)を設定する
+        # remote: client, local: server (server.py視点)
+        # オファーをremoteから受け取った際にはremoteの設定？をlocalに登録するそうな
+        await pc.setRemoteDescription(offer)
+
+        # オファーに対する回答(Answer SDP)を作成
+        # localで生成した回答をremoteに登録(注意!!：登録しただけで送信はしていない)
+        answer = await pc.createAnswer()
+        await pc.setLocalDescription(answer)
+
+        # remoteに登録した回答(Answer 'SDP')をremoteに送信する
+        return web.Response(
+            content_type="application/json",
+            text=json.dumps(
+                {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+            ),
+        )
+
+    elif params["type"] == "candidate":
+        # paramsの中身一例
+        # {'type': 'candidate', 'candidate': {'candidate': 'candidate:244898170 1 tcp 1518217471 2404:7a82:6a0:d300:f11c:8e5f:36a2:6f78 9 typ host tcptype active generation 0 ufrag QBvn network-id 3', 'sdpMid': '0', 'sdpMLineIndex': 0, 'usernameFragment': 'QBvn'}}
+        # ローカルから取得したcandidate情報をそのまま入力してもタイプ不一致でエラーを吐かれる
+        # そのためRTCIceCandidateクラスでラップする必要がある
+        # 参考：
+        # - https://aiortc.readthedocs.io/en/latest/api.html#aiortc.RTCIceCandidate
+        # - https://developer.mozilla.org/en-US/docs/Web/API/RTCIceCandidate/RTCIceCandidate
+        # - https://github.com/aiortc/aiortc/issues/1084#:~:text=async%20def%20handle_candidate(,append(rtc_candidate)
+        candidate = params["candidate"]
+        foundation = candidate["candidate"].split(' ')[0]
+        component = candidate["candidate"].split(' ')[1]
+        protocol = candidate["candidate"].split(' ')[2]
+        priority = candidate["candidate"].split(' ')[3]
+        ip = candidate["candidate"].split(' ')[4]
+        port = candidate["candidate"].split(' ')[5]
+        ctype = candidate["candidate"].split(' ')[7]
+        ice_candidate = RTCIceCandidate(
+            foundation=foundation,
+            component=component,
+            protocol=protocol,
+            priority=priority,
+            ip=ip,
+            port=port,
+            type=ctype,
+            sdpMid=candidate["sdpMid"],
+            sdpMLineIndex=candidate["sdpMLineIndex"]
+        )
+        for pc in pcs:
+            await pc.addIceCandidate(ice_candidate)
+        return web.Response(content_type="application/json", text=json.dumps({ "result": "success" }))
+    
+    else:
+        return web.Response(status=400, text="Invalid request type")
 
 
 async def on_shutdown(app):
@@ -190,10 +250,25 @@ if __name__ == "__main__":
 
     # Reactをビルドした後であればaiohttpで管理できるかも…？
     app = web.Application()
+    
     app.on_shutdown.append(on_shutdown)
+    
     # app.router.add_get("/", index)
     # app.router.add_get("/client.js", javascript)
     app.router.add_post("/offer", offer)
+    
+    # 別ソースからのアクセスだとcorsでエラーを吐かれるのでここで設定
+    # no-corsモードは制限が多いらしい(未調査)
+    cors = setup(app, defaults={
+        "*": ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+    for route in list(app.router.routes()):
+        cors.add(route)
+    
     web.run_app(
         app, access_log=None, host=args.host, port=args.port
     )
